12c8c557ca2ae69db55a6d249e749321d9430fab
Yoko Alpha <yokoo.alpha@gmail.com>
2014-01-14 22:05:35 +0000

    First commit, view controllers added to storyboard, Map view controller added, icons added.

---
+++ HealthTracker/BMIViewController.h
@@ -0,0 +1,13 @@
+//
+//  BMIViewController.h
+//  HealthTracker
+//
+//  Created by Yoko Alpha on 13/01/2014.
+//  Copyright (c) 2014 Yoko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BMIViewController : UIViewController
+
+@end
---
+++ HealthTracker/FoodTrackerViewController.h
@@ -0,0 +1,13 @@
+//
+//  FoodTrackerViewController.h
+//  HealthTracker
+//
+//  Created by Yoko Alpha on 13/01/2014.
+//  Copyright (c) 2014 Yoko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FoodTrackerViewController : UIViewController
+
+@end
