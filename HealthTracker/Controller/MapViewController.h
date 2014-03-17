9fc7913bfc5abc45dbd6ce0065812393e915dffa
Yoko Alpha <yokoo.alpha@gmail.com>
2014-03-17 18:56:11 +0000

    Added polyline recording for map

--- HealthTracker/Controller/MapViewController.h
+++ HealthTracker/Controller/MapViewController.h
@@ -6,10 +6,23 @@
 //  Copyright (c) 2014 Yoko. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import <MapKit/MapKit.h>
+@import MapKit;
+@import CoreLocation;
 
-@interface MapViewController : UIViewController <MKMapViewDelegate>
-@property (nonatomic, strong) IBOutlet MKMapView *mapHealthTracker;
-
+@interface MapViewController : UIViewController <MKMapViewDelegate, CLLocationManagerDelegate>
+{
+	MKMapView* _mapHealthTracker;
+    NSMutableArray* _points;
+	MKPolyline* _runRouteLine;
+	MKPolylineView* _runRouteLineView;
+	MKMapRect _runRouteRect;
+    CLLocationManager* _locationManager;
+    CLLocation* _currentLocation;
+}
+@property (nonatomic, strong) IBOutlet MKMapView * mapHealthTracker;
+@property (nonatomic, retain) NSMutableArray * points;
+@property (nonatomic, retain) MKPolyline * _runRouteLine;
+@property (nonatomic, retain) MKPolylineView * _runRouteLineView;
+@property (nonatomic, retain) CLLocationManager * locationManager;
+-(void) configureRoutes;
 @end
